a373721bb6da89f81b8734fb7e99efaca9c5aa53
shibli790 <shibliahmed444@gmail.com>
2025-10-25 19:02:12 +0600

    link add and rechack again

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -60,46 +60,50 @@ const Home = () => {
         </h3>
         <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {popular.map(g => (
-            <motion.div
-              key={g.id}
-              whileHover={{ scale: 1.02 }}
-              className="card gap-2 bg-[#16171b] shadow-lg shadow-[#862351] card-hover smooth"
-            >
-              <figure className="aspect-video overflow-hidden">
-                <img
-                  src={g.coverPhoto}
-                  alt={g.title}
-                  className="w-full h-full object-cover"
-                />
-              </figure>
-              <div className="p-5 ">
-                <h4 className="flex items-center text-2xl gap-2 font-bold mb-2">
-                  {g.title}
-                  <div className="text-[13px] text-[#ffffffc7] font-light px-[5px] rounded-2xl bg-gradient-to-br from-[#5653f3] to-[#605dff]">
-                    {g.category}
+            <Link to={`/games/${g.id}`}>
+              <motion.div
+                key={g.id}
+                whileHover={{ scale: 1.02 }}
+                className="card gap-2 bg-[#16171b] shadow-lg shadow-[#862351] card-hover smooth"
+              >
+                <figure className="aspect-video overflow-hidden">
+                  <img
+                    src={g.coverPhoto}
+                    alt={g.title}
+                    className="w-full h-full object-cover"
+                  />
+                </figure>
+                <div className="p-5 ">
+                  <h4 className="flex items-center text-2xl gap-2 font-bold mb-2">
+                    {g.title}
+                    <div className="text-[13px] text-[#ffffffc7] font-light px-[5px] rounded-2xl bg-gradient-to-br from-[#5653f3] to-[#605dff]">
+                      {g.category}
+                    </div>
+                  </h4>
+                  <p className="opacity-80 line-clamp-2 mb-3">
+                    {g.description}
+                  </p>
+                  <div className="card-actions justify-between items-center mt-2">
+                    <div className="flex gap-1 items-center bg-[#00d3bb] px-[5px] rounded-2xl text-[15px]">
+                      <FaStar className="text-yellow-300 text-[13px]" />
+                      {g.ratings}
+                    </div>
+                    <Link
+                      to={`/games/${g.id}`}
+                      className="btn bg-gradient-to-br from-[#f43098] to-[#a52dbd]"
+                    >
+                      View Details
+                    </Link>
                   </div>
-                </h4>
-                <p className="opacity-80 line-clamp-2 mb-3">{g.description}</p>
-                <div className="card-actions justify-between items-center mt-2">
-                  <div className="flex gap-1 items-center bg-[#00d3bb] px-[5px] rounded-2xl text-[15px]">
-                    <FaStar className="text-yellow-300 text-[13px]" />
-                    {g.ratings}
-                  </div>
-                  <Link
-                    to={`/games/${g.id}`}
-                    className="btn bg-gradient-to-br from-[#f43098] to-[#a52dbd]"
-                  >
-                    View Details
-                  </Link>
                 </div>
-              </div>
-            </motion.div>
+              </motion.div>
+            </Link>
           ))}
         </div>
       </section>
 
       {/* Newsletter */}
-      <Newsletter/>
+      <Newsletter />
     </div>
   );
 }
