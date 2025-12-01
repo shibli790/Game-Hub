6a7aa845796d1510e16a6d5318f3d703295824b6
shibli790 <shibliahmed444@gmail.com>
2025-12-01 19:54:31 +0600

    add my repo

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -6,7 +6,7 @@ export default function NotFound(){
     <div className="min-h-[60vh] grid place-items-center text-center">
       <div>
         <h1 className="text-6xl font-black tracking-tight">404</h1>
-        <p className="opacity-80 mt-2">Gamehub - A Game Library Not Fount </p>
+        <p className="opacity-80 mt-2">Gamehub - A Game Library Not Fount  </p>
         <Link to="/" className="btn btn-primary mt-4">
           Back Home
         </Link>
